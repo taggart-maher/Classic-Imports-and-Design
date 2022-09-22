5f2c356fe3526903bf6c9db20313cc0428c7fa8a
taggart-maher <taggart.talk@gmail.com>
2022-09-22 16:36:38 -0400

    try except loop

--- python webscrape/eichholtz.py
+++ python webscrape/eichholtz.py
@@ -41,108 +41,111 @@ output_file.writerow(file_header)
 fails = []
 
 for product in scrape_file:
-    product[0] = product[0].strip()
-    product[1] = product[1].strip()
-    data = []
-    #Search for product
     try:
-        driver.get('https://eichholtzusa.com/catalogsearch/result/?q=' + product[0])
-    except: 
-        fails.append(product[0] + ' Search did not succeed') 
-    #Click search result
-    try:
-        element_present = EC.presence_of_element_located((By.CLASS_NAME, 'product-item-link'))
-        WebDriverWait(driver, 6).until(element_present)
-        driver.find_elements(By.CLASS_NAME, 'product-item-link')[0].click()
-    except TimeoutException:
-        fails.append(product[0] + ' took too long to load')
-    #Wait for product page to load
-    time.sleep(3)
-    #sku
-    try:
-        info_wrap = driver.find_elements(By.CLASS_NAME, 'product-info-price')[0]
-        sku = info_wrap.find_element(By.CLASS_NAME, 'value').text
-        data.append(sku)
-    #name
-        name = driver.find_element(By.CLASS_NAME, 'page-title').text.title()
-        data.append(name)
-    except:
-        fails.append(product[0] + 'has no sku / name')
-        continue
-    #brand
-    data.append('Eichholtz')
-    #Catagories
-    data.append(product[1])
-    #wholesale
-    try:
-        wholesale = float( info_wrap.find_elements(By.TAG_NAME, 'meta')[0].get_attribute('content') )
-        data.append(wholesale)
-        #retail
-        data.append(wholesale*2.5)
-    except:
-        fails.append(product[0] + ' has no price')
-        data.append(0)
-        data.append(0)
-    #description & DIMENSIONS
-    dimensions = ''
-    product_finish = ''
-    description = ''
-    try:
-        dimensions = info_wrap.find_element(By.CLASS_NAME, 'measurement-inch').get_attribute('innerHTML') + '\n'
-    except:
-        fails.append(product[0] + ' has no dimensions')
-    try:
-        product_finish = info_wrap.find_elements(By.TAG_NAME, 'p')[0].text + '\n'
-    except:
-        fails.append(product[0] + ' has no finish')
-    try:
-        desc_wrap = driver.find_element(By.CLASS_NAME, 'product-description')
-        description = desc_wrap.find_elements(By.CLASS_NAME, 'additional-info')[0].text
-    except:
-        fails.append(product[0] + 'has no description')
-    data.append( dimensions + product_finish + description )
-    #images
-    try:
-        time.sleep(2)
-        image_list = driver.find_elements(By.CLASS_NAME, 'fotorama__nav__frame--thumb')
-        image_urls = []
-        image_filenames = []
-        c = 0
-        if not image_list:
-            image_urls.append( driver.find_element(By.CLASS_NAME, 'fotorama__loaded--img').get_attribute('href') )
-        else:
-            for pic in image_list:
-                if c != 0:
-                    pic.click()
-                    time.sleep(2)
-                image_urls.append( driver.find_element(By.CLASS_NAME, 'fotorama__active').get_attribute('href') )
-                c = c+1
-        c = 1
-        for pic in image_urls:
-            file_name = sku + '-' + str(c)
-            print(pic)
-            urllib.request.urlretrieve(pic, 'Classic-Imports-and-Design/python webscrape/product-images/' + file_name + '.webp')
-            image_filenames.append(file_name)
-            c = c + 1
-        try: 
-            pic = driver.find_element(By.CLASS_NAME, 'dimensions-image').get_attribute('src')
-            file_name = sku + '-' + str(c)
-            urllib.request.urlretrieve(pic, 'Classic-Imports-and-Design/python webscrape/product-images/' + file_name + '.png')
-            image_filenames.append(file_name)
+        product[0] = product[0].strip()
+        product[1] = product[1].strip()
+        data = []
+        #Search for product
+        try:
+            driver.get('https://eichholtzusa.com/catalogsearch/result/?q=' + product[0])
+        except: 
+            fails.append(product[0] + ' Search did not succeed') 
+        #Click search result
+        try:
+            element_present = EC.presence_of_element_located((By.CLASS_NAME, 'product-item-link'))
+            WebDriverWait(driver, 6).until(element_present)
+            driver.find_elements(By.CLASS_NAME, 'product-item-link')[0].click()
+        except TimeoutException:
+            fails.append(product[0] + ' took too long to load')
+        #Wait for product page to load
+        time.sleep(3)
+        #sku
+        try:
+            info_wrap = driver.find_elements(By.CLASS_NAME, 'product-info-price')[0]
+            sku = info_wrap.find_element(By.CLASS_NAME, 'value').text
+            data.append(sku)
+        #name
+            name = driver.find_element(By.CLASS_NAME, 'page-title').text.title()
+            data.append(name)
         except:
-            print('Standard Dimensions')
+            fails.append(product[0] + 'has no sku / name')
+            continue
+        #brand
+        data.append('Eichholtz')
+        #Catagories
+        data.append(product[1])
+        #wholesale
+        try:
+            wholesale = float( info_wrap.find_elements(By.TAG_NAME, 'meta')[0].get_attribute('content') )
+            data.append(wholesale)
+            #retail
+            data.append(wholesale*2.5)
+        except:
+            fails.append(product[0] + ' has no price')
+            data.append(0)
+            data.append(0)
+        #description & DIMENSIONS
+        dimensions = ''
+        product_finish = ''
+        description = ''
+        try:
+            dimensions = info_wrap.find_element(By.CLASS_NAME, 'measurement-inch').get_attribute('innerHTML') + '\n'
+        except:
+            fails.append(product[0] + ' has no dimensions')
+        try:
+            product_finish = info_wrap.find_elements(By.TAG_NAME, 'p')[0].text + '\n'
+        except:
+            fails.append(product[0] + ' has no finish')
+        try:
+            desc_wrap = driver.find_element(By.CLASS_NAME, 'product-description')
+            description = desc_wrap.find_elements(By.CLASS_NAME, 'additional-info')[0].text
+        except:
+            fails.append(product[0] + 'has no description')
+        data.append( dimensions + product_finish + description )
+        #images
+        try:
+            time.sleep(2)
+            image_list = driver.find_elements(By.CLASS_NAME, 'fotorama__nav__frame--thumb')
+            image_urls = []
+            image_filenames = []
+            c = 0
+            if not image_list:
+                image_urls.append( driver.find_element(By.CLASS_NAME, 'fotorama__loaded--img').get_attribute('href') )
+            else:
+                for pic in image_list:
+                    if c != 0:
+                        pic.click()
+                        time.sleep(2)
+                    image_urls.append( driver.find_element(By.CLASS_NAME, 'fotorama__active').get_attribute('href') )
+                    c = c+1
+            c = 1
+            for pic in image_urls:
+                file_name = sku + '-' + str(c)
+                print(pic)
+                urllib.request.urlretrieve(pic, 'Classic-Imports-and-Design/python webscrape/product-images/' + file_name + '.webp')
+                image_filenames.append(file_name)
+                c = c + 1
+            try: 
+                pic = driver.find_element(By.CLASS_NAME, 'dimensions-image').get_attribute('src')
+                file_name = sku + '-' + str(c)
+                urllib.request.urlretrieve(pic, 'Classic-Imports-and-Design/python webscrape/product-images/' + file_name + '.png')
+                image_filenames.append(file_name)
+            except:
+                print('Standard Dimensions')
+        except:
+            fails.append(product[0] + ' FAILED TO GRAB IMAGES')
+        
+        pp(fails)
+        data.append( ','.join(image_filenames) )
+        print_table = data.copy()
+        print_table[6] = 'desc...'
+        print_table[7] = 'images...'
+        print(tabulate([file_header] + [print_table]))
+        print(data[6])
+        print(data[7])
+        output_file.writerow(data)
     except:
-        fails.append(product[0] + ' FAILED TO GRAB IMAGES')
-    
-    pp(fails)
-    data.append( ','.join(image_filenames) )
-    print_table = data.copy()
-    print_table[6] = 'desc...'
-    print_table[7] = 'images...'
-    print(tabulate([file_header] + [print_table]))
-    print(data[6])
-    print(data[7])
-    output_file.writerow(data)
+        fails.append(product[0] + ' FAILED DUE TO UNKNOWN ERROR')
 pp(fails)
 writefile.close()
 
--- python webscrape/jonathancharles.py
+++ python webscrape/jonathancharles.py
@@ -39,84 +39,87 @@ output_file.writerow(file_header)
 fails = []
 
 for product in scrape_file:
-    product[0] = product[0].strip()
-    product[1] = product[1].strip()
-    data = []
-    driver.get('https://catalog.jonathancharlesfurniture.com/jcusa/e/mobile/products?query=' + product[0])  
     try:
-        element_present = EC.presence_of_element_located((By.CLASS_NAME, 'catalog-item-number'))
-        WebDriverWait(driver, 6).until(element_present)
-    except:
-        fails.append(product[0] + ' Does not exist')
-        continue
-    #click to product page
-    driver.find_element(By.CLASS_NAME, 'catalog-item-number').click()
-    time.sleep(3)
-    #sku
-    data.append(product[0])
-    #name
-    try:
-        product_name = driver.find_element(By.CLASS_NAME, 'item-name').find_element(By.TAG_NAME, 'h1').text
-        data.append(product_name)
-    except:
-        fails.append(product[0] + ' has NO NAME')
-        continue
-    #brand
-    data.append('Jonathan Charles')
-    #catagory
-    data.append(product[1])
-    #wholesale
-    try:
-        wholesale = float( driver.find_element(By.CLASS_NAME, 'item-price').text.lstrip('$').replace(',','') )
-        data.append(wholesale)
-    #retail
-        data.append(wholesale*2.2)
-    except:
-        data.append(0)
-        data.append(0)
-        fails.append(product[0] + ' has NO PRICE')
-    #description
-    try:
-        material = ''
-        other_mat = ''
-        info_list = driver.find_element(By.CLASS_NAME, 'label-data-list-inline').find_elements(By.TAG_NAME, 'li')
-        for element in info_list:
-            if 'Dim. - IN' in element.find_element(By.CLASS_NAME, 'label').text:
-                dimensions = element.find_element(By.CLASS_NAME, 'data').text + '\n'
-            if 'Materials' == element.find_element(By.CLASS_NAME, 'label').text:
-                material = element.find_element(By.CLASS_NAME, 'data').text + '\n'
-            if 'Other Materials' in element.find_element(By.CLASS_NAME, 'label').text:
-                other_mat = element.find_element(By.CLASS_NAME, 'data').text + '\n'   
-        description = driver.find_element(By.CSS_SELECTOR, 'p.story-full').text
-        data.append(dimensions + material + other_mat + description)
-    except:
-        data.append('NA')
-        fails.append(product[0] + 'has NO DESCRIPTION')
-    #images
-    try:
-        images = driver.find_elements(By.CSS_SELECTOR, 'div.item-image-wrapper')
-        file_names = []
-        c = 1
-        for element in images:
-            file_name = product[0] + '-' + str(c)
-            image_url = element.find_element(By.TAG_NAME, 'a').get_attribute('href')
-            print(image_url)
-            urllib.request.urlretrieve(image_url, 'Classic-Imports-and-Design/python webscrape/product-images/' + file_name + '.jpg') 
-            c = c + 1
-            file_names.append(file_name)
-        data.append(','.join(file_names))
-    except:
-        fails.append(product[0] + ' has no IMAGES')
-        data.append('NA')
+        product[0] = product[0].strip()
+        product[1] = product[1].strip()
+        data = []
+        driver.get('https://catalog.jonathancharlesfurniture.com/jcusa/e/mobile/products?query=' + product[0])  
+        try:
+            element_present = EC.presence_of_element_located((By.CLASS_NAME, 'catalog-item-number'))
+            WebDriverWait(driver, 6).until(element_present)
+        except:
+            fails.append(product[0] + ' Does not exist')
+            continue
+        #click to product page
+        driver.find_element(By.CLASS_NAME, 'catalog-item-number').click()
+        time.sleep(3)
+        #sku
+        data.append(product[0])
+        #name
+        try:
+            product_name = driver.find_element(By.CLASS_NAME, 'item-name').find_element(By.TAG_NAME, 'h1').text
+            data.append(product_name)
+        except:
+            fails.append(product[0] + ' has NO NAME')
+            continue
+        #brand
+        data.append('Jonathan Charles')
+        #catagory
+        data.append(product[1])
+        #wholesale
+        try:
+            wholesale = float( driver.find_element(By.CLASS_NAME, 'item-price').text.lstrip('$').replace(',','') )
+            data.append(wholesale)
+        #retail
+            data.append(wholesale*2.2)
+        except:
+            data.append(0)
+            data.append(0)
+            fails.append(product[0] + ' has NO PRICE')
+        #description
+        try:
+            material = ''
+            other_mat = ''
+            info_list = driver.find_element(By.CLASS_NAME, 'label-data-list-inline').find_elements(By.TAG_NAME, 'li')
+            for element in info_list:
+                if 'Dim. - IN' in element.find_element(By.CLASS_NAME, 'label').text:
+                    dimensions = element.find_element(By.CLASS_NAME, 'data').text + '\n'
+                if 'Materials' == element.find_element(By.CLASS_NAME, 'label').text:
+                    material = element.find_element(By.CLASS_NAME, 'data').text + '\n'
+                if 'Other Materials' in element.find_element(By.CLASS_NAME, 'label').text:
+                    other_mat = element.find_element(By.CLASS_NAME, 'data').text + '\n'   
+            description = driver.find_element(By.CSS_SELECTOR, 'p.story-full').text
+            data.append(dimensions + material + other_mat + description)
+        except:
+            data.append('NA')
+            fails.append(product[0] + 'has NO DESCRIPTION')
+        #images
+        try:
+            images = driver.find_elements(By.CSS_SELECTOR, 'div.item-image-wrapper')
+            file_names = []
+            c = 1
+            for element in images:
+                file_name = product[0] + '-' + str(c)
+                image_url = element.find_element(By.TAG_NAME, 'a').get_attribute('href')
+                print(image_url)
+                urllib.request.urlretrieve(image_url, 'Classic-Imports-and-Design/python webscrape/product-images/' + file_name + '.jpg') 
+                c = c + 1
+                file_names.append(file_name)
+            data.append(','.join(file_names))
+        except:
+            fails.append(product[0] + ' has no IMAGES')
+            data.append('NA')
 
-    pp(fails)
-    print_table = data.copy()
-    print_table[6] = 'desc...'
-    print_table[7] = 'images...'
-    print(tabulate([file_header] + [print_table]))
-    print(data[6])
-    print(data[7])
-    output_file.writerow(data)
+        pp(fails)
+        print_table = data.copy()
+        print_table[6] = 'desc...'
+        print_table[7] = 'images...'
+        print(tabulate([file_header] + [print_table]))
+        print(data[6])
+        print(data[7])
+        output_file.writerow(data)
+    except:
+        fails.append(product[0] + ' FAILED DUE TO UNKNOWN ERROR')
 
 pp(fails)
 writefile.close()
